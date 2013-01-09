98c3d56f594b6ada882b5e5dad5894d46ed5e4a2
John Kevin Basco <basco.johnkevin@gmail.com>
2013-01-08 23:33:52 -0800

    only show latest snippets that are already published

--- application/controllers/pages.php
+++ application/controllers/pages.php
@@ -10,7 +10,7 @@ class Pages_Controller extends Base_Controller {
 
 	public function get_index()
 	{
-		$this->layout->content = View::make('pages.index')->with('snippets', Snippet::order_by('created_at', 'desc')->take(5)->get());
+		$this->layout->content = View::make('pages.index')->with('snippets', Snippet::where_published(1)->order_by('created_at', 'desc')->take(5)->get());
 		$this->layout->currentPage = 'home';
 	}
 
